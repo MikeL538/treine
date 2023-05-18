9fe21cd0dbb08225ddeec809164bb5fc92784cb4
MikeL538 <m.lipiak97@gmail.com>
2023-05-18 21:37:10 +0200

    Update responsivness
    - Added responsivness

--- click.js
+++ click.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    body: document.querySelector("[data-body]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
